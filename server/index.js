4e7676efe6609eb5ef3870041b0daf1bace4f6a9
Viktor Prilepin <vicpride@gmail.com>
2021-03-20 16:56:51 +0700

    npm script added to start server from projectRoot

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { controller } = require('./controller');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./../'));
+app.use(express.static('./'));
 
 app.listen(PORT, function () {
    console.log(`Example app listening on port ${PORT}!`);
