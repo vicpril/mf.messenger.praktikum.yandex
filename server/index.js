1a79dd06b8d3b38f806bdd236b496642827d3905
Viktor Prilepin <vicpride@gmail.com>
2021-03-20 16:46:54 +0700

    Setup Express-server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const { controller } = require('./controller');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./../'));
+
+app.listen(PORT, function () {
+   console.log(`Example app listening on port ${PORT}!`);
+});
