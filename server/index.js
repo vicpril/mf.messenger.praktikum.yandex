7300a02c3ca757196ce488116d20635f11f476cc
Viktor Prilepin <vicpride@gmail.com>
2021-03-24 10:14:09 +0700

    Rebuild project structure

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const { controller } = require('./controller');
 
 const app = express();
 const PORT = 3000;
+const dist_folder = (__dirname + '/../client/dist/');
 
-app.use(express.static('./'));
+app.use(express.static(dist_folder));
 
 app.listen(PORT, function () {
    console.log(`Example app listening on port ${PORT}!`);
